7d407c4ddc9b0ca7916e8c65768a51b3b5b42a89
Robin Böhm <robin.boehm@workshops.de>
2017-11-13 12:57:12 +0100

    feat(course/form): add multi-select for lessons

--- lib/platform_web/controllers/course_controller.ex
+++ lib/platform_web/controllers/course_controller.ex
@@ -3,6 +3,7 @@ defmodule PlatformWeb.CourseController do
 
   alias Platform.Core
   alias Platform.Core.Schema.Course
+  alias Platform.Core.Schema.Lesson
 
   def index(conn, _params) do
     courses = Core.list_courses()
@@ -10,8 +11,8 @@ defmodule PlatformWeb.CourseController do
   end
 
   def new(conn, _params) do
-    changeset = Core.change_course(%Course{})
-    render(conn, "new.html", changeset: changeset)
+    changeset = Core.change_course(%Course{lessons: []})
+    render(conn, "new.html", changeset: changeset, collections: collections())
   end
 
   def create(conn, %{"course" => course_params}) do
@@ -21,7 +22,7 @@ defmodule PlatformWeb.CourseController do
         |> put_flash(:info, "Course created successfully.")
         |> redirect(to: course_path(conn, :show, course))
       {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "new.html", changeset: changeset)
+        render(conn, "new.html", changeset: changeset, collections: collections())
     end
   end
 
@@ -31,9 +32,9 @@ defmodule PlatformWeb.CourseController do
   end
 
   def edit(conn, %{"id" => id}) do
-    course = Core.get_course!(id)
+    course = Core.get_course_with_lessons!(id)
     changeset = Core.change_course(course)
-    render(conn, "edit.html", course: course, changeset: changeset)
+    render(conn, "edit.html", course: course, changeset: changeset, collections: collections())
   end
 
   def update(conn, %{"id" => id, "course" => course_params}) do
@@ -45,7 +46,7 @@ defmodule PlatformWeb.CourseController do
         |> put_flash(:info, "Course updated successfully.")
         |> redirect(to: course_path(conn, :show, course))
       {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "edit.html", course: course, changeset: changeset)
+        render(conn, "edit.html", course: course, changeset: changeset, collections: collections())
     end
   end
 
@@ -56,5 +57,12 @@ defmodule PlatformWeb.CourseController do
     conn
     |> put_flash(:info, "Course deleted successfully.")
     |> redirect(to: course_path(conn, :index))
+  end
+
+  # Private methods
+  defp collections do
+    %{
+      lessons: Core.list_lessons()
+    }
   end
 end
