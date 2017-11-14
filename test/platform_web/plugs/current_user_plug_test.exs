c9ea3e5eb5d1c5027f2f16ab58f53db7c8d78640
Sascha Brink <sascha.brink@gmail.com>
2017-11-14 15:46:28 +0100

    Add basic current_user_plug test

---
+++ test/platform_web/plugs/current_user_plug_test.exs
@@ -0,0 +1,21 @@
+defmodule PlatformWeb.CurrentUserPlugTest do
+  use PlatformWeb.ConnCase
+
+  alias PlatformWeb.CurrentUserPlug
+
+  describe "#init" do
+    test "passes the options unmodified" do
+      opts = %{test: 123}
+      assert CurrentUserPlug.init(opts) == opts
+    end
+  end
+
+  describe "#call" do
+    test "assigns user and token", %{conn: conn} do
+      # user = Factory.build(:user)
+      # conn = %{conn | assigns: %{current_user: user}}
+
+      # assert CurrentUserPlug.call(conn, []).assigns == ""
+    end
+  end
+end
