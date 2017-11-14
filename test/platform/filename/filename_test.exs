b1d742ed47f050923ddff97a8fdc2cf33f457231
Robin Böhm <robin.boehm@workshops.de>
2017-11-14 18:33:19 +0100

    test(filename): add basic tests for filename definitions

---
+++ test/platform/filename/filename_test.exs
@@ -0,0 +1,9 @@
+defmodule Platform.FilenameTest do
+  use ExUnit.Case
+  import Platform.Filename
+
+  alias Platform.Core.Schema.Lesson
+  alias Platform.Core.Schema.Slide
+
+  doctest Platform.Filename
+end
