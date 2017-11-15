7b79b295100241ad0a563a424765dddc05beb683
Sascha Brink <sascha.brink@gmail.com>
2017-11-15 16:20:29 +0100

    Fix adapter settings

--- lib/platform/video_converter/video_converter.ex
+++ lib/platform/video_converter/video_converter.ex
@@ -2,9 +2,9 @@ defmodule Platform.VideoConverter do
   @moduledoc """
   Context for the general converter
   """
-  @adapter Application.get_env(:platform, MODULE, [])[:adapter]
+  @adapter Application.get_env(:platform, __MODULE__, [])[:adapter]
 
-  defdelegate generate_video(opts), to: Platform.VideoConverter.FFMpegAdapter
-  defdelegate merge_videos(opts), to: Platform.VideoConverter.FFMpegAdapter
+  defdelegate generate_video(opts), to: @adapter
+  defdelegate merge_videos(opts), to: @adapter
 
 end
