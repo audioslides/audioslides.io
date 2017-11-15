eab7f2bc4b67bbb2ea6aa000329ae94e0e07feae
Sascha Brink <sascha.brink@gmail.com>
2017-11-15 16:19:13 +0100

    Use explicit setting of video adapter

--- lib/platform/video_converter/video_converter.ex
+++ lib/platform/video_converter/video_converter.ex
@@ -4,7 +4,7 @@ defmodule Platform.VideoConverter do
   """
   @adapter Application.get_env(:platform, MODULE, [])[:adapter]
 
-  defdelegate generate_video(image_filename: image_filename, audio_filename: audio_filename, output_filename: output_filename), to: @adapter
-  defdelegate merge_videos(video_filename_list: video_filename_list, output_filename: output_filename), to: @adapter
+  defdelegate generate_video(opts), to: Platform.VideoConverter.FFMpegAdapter
+  defdelegate merge_videos(opts), to: Platform.VideoConverter.FFMpegAdapter
 
 end
