a879c6b650a911fc46931d641ead7e32723a6baf
Robin Böhm <robin.boehm@workshops.de>
2017-11-15 22:55:47 +0100

    test(view_helper): add test for get_slide_image

--- lib/platform_web/helpers/view_helper.ex
+++ lib/platform_web/helpers/view_helper.ex
@@ -31,17 +31,42 @@ defmodule PlatformWeb.ViewHelper do
   def voice_gender_icon("female"), do: "👩‍🏫"
   def voice_gender_icon(_), do: "Not supported"
 
+  @doc """
+  Get the image for the first slide of a lesson.
+  If no image is available use the example image.
+
+  iex> get_course_front_slide_image(%{id: 1, slides: [%{id: 2, image_hash: "A"}]})
+  "/content/1/2.png"
+
+  iex> get_course_front_slide_image(%{id: 1, slides: [%{id: 2, image_hash: "A"}, %{id: 3, image_hash: "A"}]})
+  "/content/1/2.png"
+
+  iex> get_course_front_slide_image(%{id: 5, slides: [%{id: 100, image_hash: "B"}]})
+  "/content/5/100.png"
+
+  iex> get_course_front_slide_image(%{id: 5})
+  "/images/example-slide.png"
+
+  """
   def get_course_front_slide_image(%{slides: slides} = lesson) when length(slides) > 0 do
     slide = List.first(lesson.slides)
     get_slide_image(lesson, slide)
   end
   def get_course_front_slide_image(_), do: @example_image
 
-  def get_slide_image(lesson, slide) do
-    if slide.image_hash do
-      "/content/#{lesson.id}/#{slide.id}.png"
-    else
-      @example_image
-    end
+  @doc """
+  Get the image for the a slide.
+  If no image is available use the example image.
+
+  iex> get_slide_image(%{id: 5}, %{id: 100, image_hash: "B"})
+  "/content/5/100.png"
+
+  iex> get_slide_image(%{id: 5}, %{id: 100})
+  "/images/example-slide.png"
+
+  """
+  def get_slide_image(%{id: lesson_id} = _lesson, %{image_hash: image_hash, id: slide_id} = _slide) when is_binary(image_hash) do
+      "/content/#{lesson_id}/#{slide_id}.png"
   end
+  def get_slide_image(_lesson, _slide), do: @example_image
 end
