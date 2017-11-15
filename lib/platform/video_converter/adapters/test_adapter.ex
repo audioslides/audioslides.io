7d56760e331a4fa0c72fd426300c46791cdb3735
Robin Böhm <robin.boehm@workshops.de>
2017-11-15 16:54:09 +0100

    feat(video_converter/test_adapter): remove test IO.puts

--- lib/platform/video_converter/adapters/test_adapter.ex
+++ lib/platform/video_converter/adapters/test_adapter.ex
@@ -5,12 +5,8 @@ defmodule Platform.VideoConverter.TestAdapter do
 
   @behaviour Platform.VideoConverter
 
-  def generate_video(image_filename: _image_filename, audio_filename: _audio_filename, output_filename: _output_filename) do
-    IO.puts "TestAdapter for generate_video "
-  end
+  def generate_video(image_filename: _image_filename, audio_filename: _audio_filename, output_filename: _output_filename), do: ""
 
-  def merge_videos(video_filename_list: _video_filename_list, output_filename: _output_filename) do
-    IO.puts "TestAdapter for merge_videos "
-  end
+  def merge_videos(video_filename_list: _video_filename_list, output_filename: _output_filename), do: ""
 
 end
