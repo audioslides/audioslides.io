7dac90af754d326f9410747dba853f75a4c4a3f2
Robin Böhm <robin.boehm@workshops.de>
2017-11-15 16:15:47 +0100

    refactor(video_converter): use defdelegate

--- lib/platform/video_converter/video_converter.ex
+++ lib/platform/video_converter/video_converter.ex
@@ -2,20 +2,9 @@ defmodule Platform.VideoConverter do
   @moduledoc """
   Context for the general converter
   """
-  @adapter Application.get_env(:adapter, __MODULE__, [])
+  @adapter Application.get_env(:platform, MODULE, [])[:adapter]
 
-  def generate_video(image_filename: image_filename, audio_filename: audio_filename, output_filename: output_filename) do
-    @adapter.deliver(
-      image_filename: image_filename,
-      audio_filename: audio_filename,
-      output_filename: output_filename
-      )
-  end
-  def merge_videos(video_filename_list: video_filename_list, output_filename: output_filename) do
-    @adapter.deliver(
-      video_filename_list: video_filename_list,
-      output_filename: output_filename
-      )
-  end
+  defdelegate generate_video(image_filename: image_filename, audio_filename: audio_filename, output_filename: output_filename), to: @adapter
+  defdelegate merge_videos(video_filename_list: video_filename_list, output_filename: output_filename), to: @adapter
 
 end
