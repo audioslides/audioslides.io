0318f9979c32665670bb2b6cfc1ff4bc9310a11c
Robin Böhm <robin.boehm@workshops.de>
2017-11-16 16:04:58 +0100

    test(video): cleanup and create a setup function

--- test/platform/video/video_test.exs
+++ test/platform/video/video_test.exs
@@ -10,105 +10,58 @@ defmodule Platform.VideoTest do
 
   doctest Platform.Video
 
+  setup_with_mocks([
+    {GoogleSlides, [], [download_slide_thumb!: fn _, _, _ -> "" end]},
+    {Speech, [], [run: fn _ -> "" end, ]}])
+    do
+    lesson = Factory.insert(:lesson, google_presentation_id: "1tGbdANGoW8BGI-S-_DcP0XsXhoaTO_KConY7-RVFnkM")
+    slide_with_old_hash = Factory.insert(:slide, google_object_id: "id.g299abd206d_0_525" , page_elements_hash: "new", image_hash: "old", speaker_notes_hash: "new", audio_hash: "old" )
+    slide_up_to_date = Factory.insert(:slide, google_object_id: "id.g299abd206d_0_525" , page_elements_hash: "same_hash", image_hash: "same_hash", speaker_notes_hash: "same_hash", audio_hash: "same_hash" )
+    {:ok, lesson: lesson, slide: slide_with_old_hash, slide_up_to_date: slide_up_to_date}
+  end
+
   describe "the create_or_update_image_for_slide function" do
-    test "should call the GoogleSlide.download_slide_thumb! function when image_hash is different" do
-      with_mocks [{GoogleSlides, [], [
-        download_slide_thumb!: fn _, _, _ -> "" end
-      ]}
-      ] do
-        lesson = Factory.insert(:lesson, google_presentation_id: "1tGbdANGoW8BGI-S-_DcP0XsXhoaTO_KConY7-RVFnkM")
-        slide = Factory.insert(:slide, google_object_id: "id.g299abd206d_0_525" , page_elements_hash: "new", image_hash: "old" )
+    test "should call the GoogleSlide.download_slide_thumb! function when image_hash is different", %{lesson: lesson, slide: slide} do
+      create_or_update_image_for_slide(lesson, slide)
 
-        create_or_update_image_for_slide(lesson, slide)
-
-        assert called GoogleSlides.download_slide_thumb!(lesson.google_presentation_id, slide.google_object_id, :_ )
-      end
+      assert called GoogleSlides.download_slide_thumb!(lesson.google_presentation_id, slide.google_object_id, :_ )
     end
-    test "should not call the GoogleSlide.download_slide_thumb! function when image_hash is the same" do
-      with_mocks [{GoogleSlides, [], [
-        download_slide_thumb!: fn _, _, _ -> "" end
-      ]}
-      ] do
-        lesson = Factory.insert(:lesson, google_presentation_id: "1tGbdANGoW8BGI-S-_DcP0XsXhoaTO_KConY7-RVFnkM")
-        slide = Factory.insert(:slide, google_object_id: "id.g299abd206d_0_525" , page_elements_hash: "same_hash", image_hash: "same_hash" )
+    test "should not call the GoogleSlide.download_slide_thumb! function when image_hash is the same", %{lesson: lesson, slide_up_to_date: slide} do
+      create_or_update_image_for_slide(lesson, slide)
 
-        create_or_update_image_for_slide(lesson, slide)
-
-        assert not called GoogleSlides.download_slide_thumb!(lesson.google_presentation_id, slide.google_object_id, :_ )
-      end
+      assert not called GoogleSlides.download_slide_thumb!(lesson.google_presentation_id, slide.google_object_id, :_ )
     end
-    test "should update the image_hash after downloading the new thumb" do
-      with_mocks [{GoogleSlides, [], [
-        download_slide_thumb!: fn _, _, _ -> "" end
-      ]}
-      ] do
-        lesson = Factory.insert(:lesson, google_presentation_id: "1tGbdANGoW8BGI-S-_DcP0XsXhoaTO_KConY7-RVFnkM")
-        slide = Factory.insert(:slide, google_object_id: "id.g299abd206d_0_525" , page_elements_hash: "new", image_hash: "old" )
+    test "should update the image_hash after downloading the new thumb", %{lesson: lesson, slide: slide} do
+      create_or_update_image_for_slide(lesson, slide)
 
-        create_or_update_image_for_slide(lesson, slide)
-
-        assert called GoogleSlides.download_slide_thumb!(lesson.google_presentation_id, slide.google_object_id, :_ )
-        assert Core.get_slide!(slide.id).image_hash != slide.image_hash
-      end
+      assert called GoogleSlides.download_slide_thumb!(lesson.google_presentation_id, slide.google_object_id, :_ )
+      assert Core.get_slide!(slide.id).image_hash != slide.image_hash
     end
   end
 
 
   describe "the create_or_update_audio_for_slide function" do
-    test "should call the Speech.run function when audio_hash is different" do
-      with_mocks [
-        {Speech, [], [
-        run: fn _ -> "" end,
-      ]}
-      ] do
-        lesson = Factory.insert(:lesson, google_presentation_id: "1tGbdANGoW8BGI-S-_DcP0XsXhoaTO_KConY7-RVFnkM", voice_gender: "male", voice_language: "DE_de")
-        slide = Factory.insert(:slide, google_object_id: "id.g299abd206d_0_525" , speaker_notes_hash: "new", audio_hash: "old" )
+    test "should call the Speech.run function when audio_hash is different", %{lesson: lesson, slide: slide} do
+      create_or_update_audio_for_slide(lesson, slide)
 
-        create_or_update_audio_for_slide(lesson, slide)
-
-        assert called Speech.run(%{
-          "language_key" => lesson.voice_language,
-          "voice_gender" => lesson.voice_gender,
-          "text" => slide.speaker_notes
-        })
-      end
+      assert called Speech.run(%{
+        "language_key" => lesson.voice_language,
+        "voice_gender" => lesson.voice_gender,
+        "text" => slide.speaker_notes
+      })
     end
-    test "should not call the GoogleSlide.download_slide_thumb! function when audio_hash is the same" do
-      with_mocks [
-        {Speech, [], [
-        run: fn _ -> "" end
-      ]}
-      ] do
-        lesson = Factory.insert(:lesson, google_presentation_id: "1tGbdANGoW8BGI-S-_DcP0XsXhoaTO_KConY7-RVFnkM", voice_gender: "male", voice_language: "DE_de")
-        slide = Factory.insert(:slide, google_object_id: "id.g299abd206d_0_525" , speaker_notes_hash: "same_hash", audio_hash: "same_hash", speaker_notes: "some text" )
+    test "should not call the GoogleSlide.download_slide_thumb! function when audio_hash is the same", %{lesson: lesson, slide_up_to_date: slide} do
+      create_or_update_audio_for_slide(lesson, slide)
 
-        create_or_update_audio_for_slide(lesson, slide)
-
-        assert not called Speech.run(%{
-          "language_key" => lesson.voice_language,
-          "voice_gender" => lesson.voice_gender,
-          "text" => slide.speaker_notes
-        })
-      end
+      assert not called Speech.run(%{
+        "language_key" => lesson.voice_language,
+        "voice_gender" => lesson.voice_gender,
+        "text" => slide.speaker_notes
+      })
     end
-    test "should update the audio_hash after downloading the new thumb" do
-      with_mocks [
-        {Speech, [], [
-          run: fn _ -> <<12, 13, 14>> end
-       ]}
-      ] do
-        lesson = Factory.insert(:lesson, google_presentation_id: "1tGbdANGoW8BGI-S-_DcP0XsXhoaTO_KConY7-RVFnkM", voice_gender: "male", voice_language: "DE_de")
-        slide = Factory.insert(:slide, google_object_id: "id.g299abd206d_0_525" , speaker_notes_hash: "new", audio_hash: "old", speaker_notes: "some text" )
-
-        create_or_update_audio_for_slide(lesson, slide)
-
-        assert called Speech.run(%{
-          "language_key" => lesson.voice_language,
-          "voice_gender" => lesson.voice_gender,
-          "text" => slide.speaker_notes
-        })
-        assert Core.get_slide!(slide.id).audio_hash == "new"
-      end
+    test "should update the audio_hash after downloading the new thumb", %{lesson: lesson, slide: slide} do
+      create_or_update_audio_for_slide(lesson, slide)
+      assert Core.get_slide!(slide.id).audio_hash == "new"
     end
   end
 end
