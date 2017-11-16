7188875e6f3eb3353368c77c138f0aafe7e81bfe
Robin Böhm <robin.boehm@workshops.de>
2017-11-16 16:56:16 +0100

    test(file_helper): add test for write_to_file with mocks

---
+++ test/platform/file_helper/file_helper_test.exs
@@ -0,0 +1,33 @@
+defmodule Platform.FileHelperTest do
+  use ExUnit.Case, async: false
+  import Mock
+  import Platform.FileHelper
+
+  setup_with_mocks([
+    {File, [], [
+      rm: fn _ -> true end,
+      close: fn _ -> true end,
+      open: fn _out, _opts -> {:ok, nil} end,
+      mkdir_p: fn _ -> true end
+      ]},
+    {IO, [:passthrough], [binwrite: fn _file, _content -> true end]}
+    ])
+  do
+    {:ok, filename: "/example/dir/filename.ext", data: "exampleData"}
+  end
+
+  describe "the write_to_file function" do
+    test "should create the directory for the file", %{filename: filename, data: data} do
+      write_to_file(filename, data)
+
+      assert called File.mkdir_p("/example/dir/")
+    end
+    test "should write the content to the file", %{filename: filename, data: data} do
+      write_to_file(filename, data)
+
+      assert called File.open("filename.ext", [:write])
+      assert called IO.binwrite(:_, data)
+      assert called File.close(:_)
+    end
+  end
+end
