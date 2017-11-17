edbff18b3b0f595cf78c62acd47bdf947c402ef3
Robin Böhm <robin.boehm@workshops.de>
2017-11-17 13:06:01 +0100

    test(current_user_plug): add test for #call/2

--- test/platform_web/plugs/current_user_plug_test.exs
+++ test/platform_web/plugs/current_user_plug_test.exs
@@ -1,7 +1,10 @@
 defmodule PlatformWeb.CurrentUserPlugTest do
   use PlatformWeb.ConnCase
+  use Plug.Test
 
   alias PlatformWeb.CurrentUserPlug
+
+  import Mock
 
   describe "#init" do
     test "passes the options unmodified" do
@@ -11,11 +14,32 @@ defmodule PlatformWeb.CurrentUserPlugTest do
   end
 
   describe "#call" do
-    test "assigns user and token", %{conn: _conn} do
-      # user = Factory.build(:user)
-      # conn = %{conn | assigns: %{current_user: user}}
+    test "assigns user and token when user_id is given", %{conn: conn} do
+      with_mock Phoenix.Token, [sign: fn _, _, _ -> "MYTOKEN" end] do
+        user = Factory.insert(:user)
 
-      # assert CurrentUserPlug.call(conn, []).assigns == ""
+        conn = conn
+          |> init_test_session(%{user_id: user.id})
+          |> put_session(:user_id, user.id) # raises ** (ArgumentError) session not fetched, call fetch_session/2
+          |> CurrentUserPlug.call([])
+
+        assert conn.assigns.current_user.id == user.id
+        assert conn.assigns.user_token == "MYTOKEN"
+      end
+    end
+
+    test "assigns user and token when current_user is given", %{conn: conn} do
+      with_mock Phoenix.Token, [sign: fn _, _, _ -> "MYTOKEN" end] do
+        user = Factory.insert(:user)
+        conn = %{conn | assigns: %{current_user: user}}
+
+
+        conn = conn
+          |> CurrentUserPlug.call([])
+
+        assert conn.assigns.current_user.id == user.id
+        assert conn.assigns.user_token == "MYTOKEN"
+      end
     end
   end
 end
