3332b26d0f192af0735d77052f6f1b5b0c228728
Robin Böhm <robin.boehm@workshops.de>
2017-11-17 16:13:00 +0100

    test(lesson_sync): write a test for create_or_update_slides(%Lesson{})

--- test/platform/core/lesson_sync_test.exs
+++ test/platform/core/lesson_sync_test.exs
@@ -1,8 +1,11 @@
 defmodule Platform.LessonSyncTest do
   use Platform.DataCase
 
+  import Mock
+
   # alias Platform.Core.Schema.Lesson
   alias Platform.Core.LessonSync
+  alias Platform.GoogleSlides
   alias Platform.GoogleSlidesFactory
   alias Platform.Factory
 
@@ -48,5 +51,31 @@ defmodule Platform.LessonSyncTest do
       assert length(lesson.slides) == 1
       assert Enum.map(lesson.slides, fn(slide) -> slide.google_object_id end) == ["objID_1"]
     end
+  end
+
+
+  describe "create_or_update_slides" do
+    test "should fetch a google preentation and sync with %Lesson{} as parameter" do
+      with_mock GoogleSlides, [:passthrough], [get_presentation!: fn _ -> create_google_lesson() end] do
+
+        lesson = Factory.insert(:lesson)
+
+        LessonSync.sync_slides(lesson)
+
+        assert called GoogleSlides.get_presentation!(lesson.google_presentation_id)
+      end
+    end
+  end
+
+  defp create_google_lesson do
+    lesson = Factory.insert(:lesson)
+    Factory.insert(:slide, lesson: lesson)
+
+    slide1 = GoogleSlidesFactory.get_base_slide(object_id: "objID_1", content: "Example Content 1", speaker_notes: "Speaker Notes 1")
+
+    %GoogleApi.Slides.V1.Model.Presentation{
+      presentationId: lesson.google_presentation_id,
+      slides: [slide1]
+    }
   end
 end
