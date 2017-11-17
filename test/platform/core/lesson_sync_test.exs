bd8a9a9b56be6c44f5c230b856f8b123553c9e9b
Robin Böhm <robin.boehm@workshops.de>
2017-11-17 16:24:43 +0100

    test(lesson_sync): create a setup function

--- test/platform/core/lesson_sync_test.exs
+++ test/platform/core/lesson_sync_test.exs
@@ -9,19 +9,22 @@ defmodule Platform.LessonSyncTest do
   alias Platform.GoogleSlidesFactory
   alias Platform.Factory
 
+  setup do
+    lesson = Factory.insert(:lesson)
+
+    google_slide1 = GoogleSlidesFactory.get_base_slide(object_id: "objID_1", content: "Example Content 1", speaker_notes: "Speaker Notes 1")
+    google_slide2 = GoogleSlidesFactory.get_base_slide(object_id: "objID_2", content: "Example Content 2", speaker_notes: "Speaker Notes 2")
+
+    google_presentation = %GoogleApi.Slides.V1.Model.Presentation{
+      presentationId: lesson.google_presentation_id,
+      slides: [google_slide1, google_slide2]
+    }
+    {:ok, %{lesson: lesson, google_presentation: google_presentation}}
+  end
+
   describe "#sync_slides" do
-    test "should insert slides if they don't exist" do
-      lesson = Factory.insert(:lesson)
-
-      google_slide1 = GoogleSlidesFactory.get_base_slide(object_id: "objID_1", content: "Example Content 1", speaker_notes: "Speaker Notes 1")
-      google_slide2 = GoogleSlidesFactory.get_base_slide(object_id: "objID_2", content: "Example Content 2", speaker_notes: "Speaker Notes 2")
-
-      google_lesson = %GoogleApi.Slides.V1.Model.Presentation{
-        presentationId: lesson.google_presentation_id,
-        slides: [google_slide1, google_slide2]
-      }
-
-      LessonSync.sync_slides(google_lesson)
+    test "should insert slides if they don't exist", %{lesson: lesson, google_presentation: google_presentation} do
+      LessonSync.sync_slides(google_presentation)
 
       lesson =
         lesson
@@ -37,12 +40,12 @@ defmodule Platform.LessonSyncTest do
 
       slide1 = GoogleSlidesFactory.get_base_slide(object_id: "objID_1", content: "Example Content 1", speaker_notes: "Speaker Notes 1")
 
-      google_lesson = %GoogleApi.Slides.V1.Model.Presentation{
+      google_presentation = %GoogleApi.Slides.V1.Model.Presentation{
         presentationId: lesson.google_presentation_id,
         slides: [slide1]
       }
 
-      LessonSync.sync_slides(google_lesson)
+      LessonSync.sync_slides(google_presentation)
 
       lesson =
         lesson
@@ -55,27 +58,26 @@ defmodule Platform.LessonSyncTest do
 
 
   describe "create_or_update_slides" do
-    test "should fetch a google preentation and sync with %Lesson{} as parameter" do
-      with_mock GoogleSlides, [:passthrough], [get_presentation!: fn _ -> create_google_lesson() end] do
-
-        lesson = Factory.insert(:lesson)
-
+    test "should fetch a google presentation and sync with %Lesson{} as parameter", %{lesson: lesson, google_presentation: google_presentation} do
+      with_mock GoogleSlides, [:passthrough], [get_presentation!: fn _ -> google_presentation end] do
         LessonSync.sync_slides(lesson)
 
         assert called GoogleSlides.get_presentation!(lesson.google_presentation_id)
       end
     end
-  end
 
-  defp create_google_lesson do
-    lesson = Factory.insert(:lesson)
-    Factory.insert(:slide, lesson: lesson)
+    test "should insert slides if they don't exist with a lesson as input", %{lesson: lesson, google_presentation: google_presentation} do
+      with_mock GoogleSlides, [:passthrough], [get_presentation!: fn _ -> google_presentation end] do
+        LessonSync.sync_slides(lesson)
 
-    slide1 = GoogleSlidesFactory.get_base_slide(object_id: "objID_1", content: "Example Content 1", speaker_notes: "Speaker Notes 1")
+        lesson =
+          lesson
+          |> Repo.preload(:slides)
 
-    %GoogleApi.Slides.V1.Model.Presentation{
-      presentationId: lesson.google_presentation_id,
-      slides: [slide1]
-    }
+        assert length(lesson.slides) == 2
+        assert Enum.map(lesson.slides, fn(slide) -> slide.google_object_id end) == ["objID_1", "objID_2"]
+      end
+    end
+
   end
 end
