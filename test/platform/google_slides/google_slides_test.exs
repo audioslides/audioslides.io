7bc52428b081e20db405f46b7783990c942b2aea
Robin Böhm <robin.boehm@workshops.de>
2017-11-17 11:29:36 +0100

    test(google_slide): add test for download_thumb

--- test/platform/google_slides/google_slides_test.exs
+++ test/platform/google_slides/google_slides_test.exs
@@ -9,9 +9,9 @@ defmodule Platform.GoogleSlidesTest do
   alias Goth.Token
   alias GoogleApi.Slides.V1.Api.Presentations
   alias GoogleApi.Slides.V1.Connection
-
   alias GoogleApi.Slides.V1.Model.Page
   alias GoogleApi.Slides.V1.Model.PageElement
+  alias Platform.FileHelper
 
   import Platform.GoogleSlidesFactory
   import Platform.GoogleSlides
@@ -22,11 +22,13 @@ defmodule Platform.GoogleSlidesTest do
       {Presentations, [], [
         slides_presentations_get: fn _con, _presentation_id, _opts -> {:ok, %{}} end,
         slides_presentations_pages_get: fn _con, _presentation_id, _slide_id -> {:ok, %{}} end,
-        slides_presentations_pages_get_thumbnail: fn _con, _presentation_id, _slide_id -> {:ok, %{}} end,
+        slides_presentations_pages_get_thumbnail: fn _con, _presentation_id, _slide_id -> {:ok, %{contentUrl: "https://some.content/for/you.png"}} end,
 
         ]},
       {Connection, [], [new: fn _ -> %{} end]},
-      {Token, [], [for_scope: fn _ -> {:ok, %{token: "SUPER_TOKEN"}} end]}
+      {Token, [], [for_scope: fn _ -> {:ok, %{token: "SUPER_TOKEN"}} end]},
+      {FileHelper, [], [write_to_file: fn _, _ -> {:ok, "mock file written"} end]},
+      {HTTPoison, [], [get!: fn _url -> %HTTPoison.Response{body: "data"} end]}
     ])
     do
     {:ok, nothing: "yes"}
@@ -53,6 +55,14 @@ defmodule Platform.GoogleSlidesTest do
       get_slide_thumb!("presentation_id","slide_id")
 
       assert called Presentations.slides_presentations_pages_get_thumbnail(:_, "presentation_id", "slide_id")
+    end
+  end
+
+  describe "download_slide_thumb!" do
+    test "should get a slide via GoogleAPI" do
+      download_slide_thumb!("presentation_id","slide_id", "file/name/example.png")
+
+      assert called FileHelper.write_to_file("file/name/example.png", "data")
     end
   end
 
