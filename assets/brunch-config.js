3e926d59baf83f05a8bb8512ac99ea91740aa757
Robin Böhm <robin.boehm@workshops.de>
2017-11-22 14:38:26 +0100

    feat(web): add font-awesome

--- assets/brunch-config.js
+++ assets/brunch-config.js
@@ -20,7 +20,10 @@ exports.config = {
       // }
     },
     stylesheets: {
-      joinTo: "css/app.css"
+      joinTo: "css/app.css",
+      order: {
+        after: ["priv/static/css/app.scss"] // concat app.css last
+      }
     },
     templates: {
       joinTo: "js/app.js"
@@ -47,6 +50,17 @@ exports.config = {
     babel: {
       // Do not use ES6 compiler in vendor code
       ignore: [/vendor/]
+    },
+    copycat: {
+      "fonts": ["node_modules/font-awesome/fonts"]
+    },
+    sass: {
+      options: {
+        includePaths: [
+          "node_modules/bootstrap/scss",
+          "node_modules/font-awesome/scss"
+        ]
+      }
     }
   },
 
