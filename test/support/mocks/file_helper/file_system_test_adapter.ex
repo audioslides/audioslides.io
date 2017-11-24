41159ce1ad85bcb669418ab04b8afca6cb8401fe
Robin Böhm <robin.boehm@workshops.de>
2017-11-24 11:30:02 +0100

    feat(file_helper_test): add Logger.info to FileSystemAdapter

--- test/support/mocks/file_helper/file_system_test_adapter.ex
+++ test/support/mocks/file_helper/file_system_test_adapter.ex
@@ -3,10 +3,12 @@ defmodule Platform.FileHelper.FileSystemTestAdapter do
   Simple
   """
 
+  require Logger
+
   @behaviour Platform.FileHelper
 
-  def write_to_file(_filename, _data) do
-    #IO.puts "data written to #{filename}"
+  def write_to_file(filename, _data) do
+    Logger.info "#{__MODULE__}: Some data would be written in #{filename}"
   end
 
 end
