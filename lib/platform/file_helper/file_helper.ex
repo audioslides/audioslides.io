02817d3bd2e29e377aa202116b3160efaac90a58
Robin Böhm <robin.boehm@workshops.de>
2017-11-28 10:04:57 +0100

    feat(file_helper): extend file_helper behaviour with remove_file and remove_folder

--- lib/platform/file_helper/file_helper.ex
+++ lib/platform/file_helper/file_helper.ex
@@ -11,4 +11,18 @@ defmodule Platform.FileHelper do
   @callback write_to_file(String.t, String.t) :: {:ok, String.t} | {:error, String.t}
   defdelegate write_to_file(filename, data), to: @adapter
 
+  @doc """
+  Remove a file
+
+  """
+  @callback remove_file(String.t) :: {:ok, String.t} | {:error, String.t}
+  defdelegate remove_file(filename), to: @adapter
+
+  @doc """
+  Remove a folder
+
+  """
+  @callback remove_folder(String.t) :: {:ok, String.t} | {:error, String.t}
+  defdelegate remove_folder(filename), to: @adapter
+
 end
