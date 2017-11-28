386acbd67a8142c6095f917168f14753d9834b60
Robin Böhm <robin.boehm@workshops.de>
2017-11-28 10:06:49 +0100

    refactor(file_system_adapter_test): rename file

--- test/platform/file_helper/file_system_adapter_test.exs
+++ test/platform/file_helper/file_system_adapter_test.exs
@@ -1,4 +1,4 @@
-defmodule Platform.FileHelperTest do
+defmodule Platform.FileSystemAdapterTest do
   use ExUnit.Case, async: false
   import Mock
   import Platform.FileHelper.FileSystemAdapter
@@ -6,6 +6,7 @@ defmodule Platform.FileHelperTest do
   setup_with_mocks([
     {File, [], [
       rm: fn _ -> true end,
+      rm_rf: fn _ -> true end,
       close: fn _ -> true end,
       open: fn _out, _opts -> {:ok, nil} end,
       mkdir_p: fn _ -> true end
@@ -28,6 +29,22 @@ defmodule Platform.FileHelperTest do
       assert called File.open(filename, [:write])
       assert called IO.binwrite(:_, data)
       assert called File.close(:_)
+    end
+  end
+
+  describe "remove_file/1" do
+    test "should remove a file", %{filename: filename} do
+      remove_file(filename)
+
+      assert called File.rm(filename)
+    end
+  end
+
+  describe "remove_folder/1" do
+    test "should remove the directory", %{filename: filename} do
+      remove_folder(filename)
+
+      assert called File.rm_rf(filename)
     end
   end
 end
