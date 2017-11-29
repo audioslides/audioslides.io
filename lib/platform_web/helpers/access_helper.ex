8803fc8a1ca04d12ad56e8c3b7d2cf1182f98ee1
Robin Böhm <robin.boehm@workshops.de>
2017-11-29 11:29:51 +0100

    fix(access_helper): change atom for list_lessons/1 to :list_all_lessons

--- lib/platform_web/helpers/access_helper.ex
+++ lib/platform_web/helpers/access_helper.ex
@@ -6,7 +6,7 @@ defmodule PlatformWeb.AccessHelper do
   alias Platform.Core
 
   def list_lessons(conn) do
-    if can?(conn, :show, User) do
+    if can?(conn, :list_all_lessons, Lesson) do
       Core.list_lessons()
     else
       Core.list_visible_lessons()
