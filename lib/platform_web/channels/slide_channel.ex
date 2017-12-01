9bcdef2c1fec6dc03e04628f17f5ea909187a527
Sascha Brink <sascha.brink@gmail.com>
2017-12-01 12:08:27 +0100

    Refactor channel broadcast

--- lib/platform_web/channels/slide_channel.ex
+++ lib/platform_web/channels/slide_channel.ex
@@ -6,19 +6,14 @@ defmodule PlatformWeb.SlideChannel do
 
   alias Platform.Speech
 
-  @endpoint PlatformWeb.Endpoint # move this to a config option
-
   def join("slide", _payload, socket) do
     {:ok, socket}
   end
 
-  # It is also common to receive messages from the client and
-  # broadcast to everyone in the current topic (room:lobby).
   def handle_in("speech", payload, socket) do
     preview_url = Speech.get_speech_url(payload)
 
-    @endpoint.broadcast! "slide", "speech", %{"preview_url"=> preview_url}
-
+    broadcast! socket, "speech", %{"preview_url"=> preview_url}
     {:noreply, socket}
   end
 end
