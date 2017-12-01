02ddf06d1d6d71a636568650a411c23f70e0c623
Sascha Brink <sascha.brink@gmail.com>
2017-12-01 11:25:43 +0100

    Get selection from current word till end

--- assets/js/speaker-notes-editor.js
+++ assets/js/speaker-notes-editor.js
@@ -4,7 +4,7 @@ import CodeMirror from "codemirror";
 jQuery(document).ready(function () {
   let elem = document.getElementById('slide_speaker_notes');
 
-  CodeMirror.fromTextArea(elem, {
+  let editor = CodeMirror.fromTextArea(elem, {
     // mode: "markdown",
     theme: "default",
     lineNumbers: false,
@@ -12,5 +12,17 @@ jQuery(document).ready(function () {
     tabSize: 2,
     tabMode: "indent",
     viewportMargin: Infinity,
+  });
+
+  editor.on('cursorActivity', function(){
+    var A1 = editor.getCursor().line;
+    var A2 = editor.getCursor().ch;
+
+    var lastLine = editor.lastLine()
+    var lastChar = editor.getLine(lastLine).length
+
+    var B1 = editor.findWordAt({line: A1, ch: A2}).anchor.ch;
+
+    console.log(editor.getRange({line: A1,ch: B1}, {line: lastLine, ch: lastChar}));
   });
 });
