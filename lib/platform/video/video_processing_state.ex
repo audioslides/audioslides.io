fa55a5d80916949153f4eea849a650fb3c424dac
Robin Böhm <robin.boehm@workshops.de>
2017-12-01 17:08:40 +0100

    refactor(video_processing_state): remove "inital" from function names

--- lib/platform/video/video_processing_state.ex
+++ lib/platform/video/video_processing_state.ex
@@ -7,56 +7,56 @@ defmodule Platform.VideoProcessingState do
   alias Platform.Core.Schema.Slide
   alias Platform.Video
 
-  def get_initial_processing_state(%Lesson{slides: slides} = lesson) when is_list(slides) do
+  def get_processing_state(%Lesson{slides: slides} = lesson) when is_list(slides) do
     %{
      lesson_id: lesson.id,
-     video_state: get_initial_state_for_lesson_video(lesson),
-     slides: Enum.map(slides, fn slide -> get_initial_processing_state_for_slide(slide) end)
+     video_state: get_state_for_lesson_video(lesson),
+     slides: Enum.map(slides, fn slide -> get_processing_state_for_slide(slide) end)
     }
   end
-  def get_initial_processing_state(%Lesson{slides: slides} = lesson) when not is_list(slides) do
+  def get_processing_state(%Lesson{slides: slides} = lesson) when not is_list(slides) do
     %{
      lesson_id: lesson.id,
-     video_state: get_initial_state_for_lesson_video(lesson),
+     video_state: get_state_for_lesson_video(lesson),
      slides: []
     }
   end
 
-  def get_initial_state_for_lesson_video(%Lesson{video_hash: nil}), do: "NEW"
-  def get_initial_state_for_lesson_video(%Lesson{video_hash: video_hash} = lesson) do
+  def get_state_for_lesson_video(%Lesson{video_hash: nil}), do: "NEW"
+  def get_state_for_lesson_video(%Lesson{video_hash: video_hash} = lesson) do
     case Video.generate_video_hash(lesson) == video_hash do
       true -> "NO_UPDATED_NEEDED"
       false -> "NEED_UPDATE"
     end
   end
 
-  def get_initial_processing_state_for_slide(%Slide{} = slide) do
+  def get_processing_state_for_slide(%Slide{} = slide) do
     %{
       slide_id: slide.id,
-      video_state: get_initial_state_for_slide_video(slide),
-      audio_state: get_initial_state_for_slide_audio(slide),
-      image_state: get_initial_state_for_slide_image(slide)
+      video_state: get_state_for_slide_video(slide),
+      audio_state: get_state_for_slide_audio(slide),
+      image_state: get_state_for_slide_image(slide)
     }
   end
 
-  def get_initial_state_for_slide_audio(%Slide{audio_hash: nil}), do: "NEW"
-  def get_initial_state_for_slide_audio(%Slide{audio_hash: audio_hash, speaker_notes_hash: speaker_notes_hash}) do
+  def get_state_for_slide_audio(%Slide{audio_hash: nil}), do: "NEW"
+  def get_state_for_slide_audio(%Slide{audio_hash: audio_hash, speaker_notes_hash: speaker_notes_hash}) do
     case speaker_notes_hash == audio_hash do
       true -> "NO_UPDATED_NEEDED"
       false -> "NEED_UPDATE"
     end
   end
 
-  def get_initial_state_for_slide_image(%Slide{image_hash: nil}), do: "NEW"
-  def get_initial_state_for_slide_image(%Slide{image_hash: image_hash, page_elements_hash: page_elements_hash}) do
+  def get_state_for_slide_image(%Slide{image_hash: nil}), do: "NEW"
+  def get_state_for_slide_image(%Slide{image_hash: image_hash, page_elements_hash: page_elements_hash}) do
     case page_elements_hash == image_hash do
       true -> "NO_UPDATED_NEEDED"
       false -> "NEED_UPDATE"
     end
   end
 
-  def get_initial_state_for_slide_video(%Slide{video_hash: nil}), do: "NEW"
-  def get_initial_state_for_slide_video(%Slide{video_hash: video_hash} = slide) do
+  def get_state_for_slide_video(%Slide{video_hash: nil}), do: "NEW"
+  def get_state_for_slide_video(%Slide{video_hash: video_hash} = slide) do
     case Video.generate_video_hash(slide) == video_hash do
       true -> "NO_UPDATED_NEEDED"
       false -> "NEED_UPDATE"
--- test/platform/video/video_processing_state_test.exs
+++ test/platform/video/video_processing_state_test.exs
@@ -7,18 +7,18 @@ defmodule Platform.VideoProcessingStateTest do
 
   doctest Platform.VideoProcessingState
 
-  describe "get_initial_processing_state/1" do
+  describe "get_processing_state/1" do
     test "should set lesson_id" do
       lesson = Factory.insert(:lesson)
 
-      result = get_initial_processing_state(lesson)
+      result = get_processing_state(lesson)
       assert result.lesson_id == lesson.id
     end
 
     test "should set video_state to NEW if no video is genereted by now" do
       lesson = Factory.insert(:lesson, video_hash: nil)
 
-      result = get_initial_processing_state(lesson)
+      result = get_processing_state(lesson)
       assert result.video_state == "NEW"
     end
 
@@ -27,7 +27,7 @@ defmodule Platform.VideoProcessingStateTest do
       slide2 = Factory.build(:slide, video_hash: "OTHER_HASH")
       lesson = Factory.build(:lesson, video_hash: Video.sha256("SAME_HASHSAME_HASH"), slides: [slide1, slide2])
 
-      result = get_initial_processing_state(lesson)
+      result = get_processing_state(lesson)
       assert result.video_state == "NEED_UPDATE"
     end
 
@@ -36,7 +36,7 @@ defmodule Platform.VideoProcessingStateTest do
       slide2 = Factory.build(:slide, video_hash: "SAME_HASH")
       lesson = Factory.build(:lesson, video_hash: Video.sha256("SAME_HASHSAME_HASH"), slides: [slide1, slide2])
 
-      result = get_initial_processing_state(lesson)
+      result = get_processing_state(lesson)
       assert result.video_state == "NO_UPDATED_NEEDED"
     end
 
@@ -44,16 +44,16 @@ defmodule Platform.VideoProcessingStateTest do
       slide = Factory.build(:slide)
       lesson = Factory.build(:lesson, slides: [slide])
 
-      result = get_initial_processing_state(lesson)
+      result = get_processing_state(lesson)
       assert length(result.slides) == 1
     end
   end
 
-  describe "get_initial_processing_state_for_slide/1" do
+  describe "get_processing_state_for_slide/1" do
     test "should set slide_id" do
       slide = Factory.insert(:slide)
 
-      result = get_initial_processing_state_for_slide(slide)
+      result = get_processing_state_for_slide(slide)
       assert result.slide_id == slide.id
     end
 
@@ -61,21 +61,21 @@ defmodule Platform.VideoProcessingStateTest do
     test "should set video_state to NEW if no video is genereted by now" do
       slide = Factory.insert(:slide, video_hash: nil)
 
-      result = get_initial_processing_state_for_slide(slide)
+      result = get_processing_state_for_slide(slide)
       assert result.video_state == "NEW"
     end
 
     test "should set video_state to NO_UPDATED_NEEDED if speaker_notes_hash is same" do
       slide = Factory.insert(:slide, audio_hash: "SAME_HASH", image_hash: "SAME_HASH", video_hash: Video.sha256("SAME_HASHSAME_HASH"))
 
-      result = get_initial_processing_state_for_slide(slide)
+      result = get_processing_state_for_slide(slide)
       assert result.video_state == "NO_UPDATED_NEEDED"
     end
 
     test "should set video_state to NEED_UPDATE if speaker_notes_hash is different" do
       slide = Factory.insert(:slide, audio_hash: "SOME_HASH", image_hash: "SOME_OTHER_HASH", video_hash: Video.sha256("SOME_HASHSOME_HASH"))
 
-      result = get_initial_processing_state_for_slide(slide)
+      result = get_processing_state_for_slide(slide)
       assert result.video_state == "NEED_UPDATE"
     end
 
@@ -83,21 +83,21 @@ defmodule Platform.VideoProcessingStateTest do
     test "should set audio_state to NEW if no audio is genereted by now" do
       slide = Factory.insert(:slide, audio_hash: nil)
 
-      result = get_initial_processing_state_for_slide(slide)
+      result = get_processing_state_for_slide(slide)
       assert result.audio_state == "NEW"
     end
 
     test "should set audio_state to NO_UPDATED_NEEDED if speaker_notes_hash is same" do
       slide = Factory.insert(:slide, audio_hash: "SAME_HASH", speaker_notes_hash: "SAME_HASH")
 
-      result = get_initial_processing_state_for_slide(slide)
+      result = get_processing_state_for_slide(slide)
       assert result.audio_state == "NO_UPDATED_NEEDED"
     end
 
     test "should set audio_state to NEED_UPDATE if speaker_notes_hash is different" do
       slide = Factory.insert(:slide, audio_hash: "SOME_HASH", speaker_notes_hash: "SOME_OTHER_HASH")
 
-      result = get_initial_processing_state_for_slide(slide)
+      result = get_processing_state_for_slide(slide)
       assert result.audio_state == "NEED_UPDATE"
     end
 
@@ -105,21 +105,21 @@ defmodule Platform.VideoProcessingStateTest do
     test "should set image_state to NEW if no image is genereted by now" do
       slide = Factory.insert(:slide, image_hash: nil)
 
-      result = get_initial_processing_state_for_slide(slide)
+      result = get_processing_state_for_slide(slide)
       assert result.image_state == "NEW"
     end
 
     test "should set image_state to NO_UPDATED_NEEDED if page_elements_hash is same" do
       slide = Factory.insert(:slide, image_hash: "SAME_HASH", page_elements_hash: "SAME_HASH")
 
-      result = get_initial_processing_state_for_slide(slide)
+      result = get_processing_state_for_slide(slide)
       assert result.image_state == "NO_UPDATED_NEEDED"
     end
 
     test "should set image_state to NEED_UPDATE if page_elements_hash is different" do
       slide = Factory.insert(:slide, image_hash: "SOME_HASH", page_elements_hash: "SOME_OTHER_HASH")
 
-      result = get_initial_processing_state_for_slide(slide)
+      result = get_processing_state_for_slide(slide)
       assert result.image_state == "NEED_UPDATE"
     end
   end
