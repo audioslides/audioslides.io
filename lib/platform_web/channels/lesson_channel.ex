895d808e3958327395b1f15854a2f646d470ad1b
Sascha Brink <sascha.brink@gmail.com>
2017-12-05 14:54:55 +0100

    Fix warning

--- lib/platform_web/channels/lesson_channel.ex
+++ lib/platform_web/channels/lesson_channel.ex
@@ -4,7 +4,7 @@ defmodule PlatformWeb.LessonChannel do
   """
   use PlatformWeb, :channel
 
-  def join("lesson:" <> lesson_id, _payload, socket) do
+  def join("lesson:" <> _lesson_id, _payload, socket) do
     # socket_with_lesson_id =
     #   socket
     #   |> assign(:lesson_id, lesson_id)
