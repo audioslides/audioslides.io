5e9b4c9fbde1bd3d03f48401264e3f6e849b36bf
Sascha Brink <sascha.brink@gmail.com>
2017-12-05 10:38:29 +0100

    Remove unused lesson channel tests

--- test/platform_web/channels/lesson_channel_test.exs
+++ test/platform_web/channels/lesson_channel_test.exs
@@ -11,18 +11,18 @@ defmodule PlatformWeb.LessonChannelTest do
     {:ok, socket: socket}
   end
 
-  test "ping replies with status ok", %{socket: socket} do
-    ref = push socket, "ping", %{"hello" => "there"}
-    assert_reply ref, :ok, %{"hello" => "there"}
-  end
+  # test "ping replies with status ok", %{socket: socket} do
+  #   ref = push socket, "ping", %{"hello" => "there"}
+  #   assert_reply ref, :ok, %{"hello" => "there"}
+  # end
 
-  test "shout broadcasts to lesson:lobby", %{socket: socket} do
-    push socket, "shout", %{"hello" => "all"}
-    assert_broadcast "shout", %{"hello" => "all"}
-  end
+  # test "shout broadcasts to lesson:lobby", %{socket: socket} do
+  #   push socket, "shout", %{"hello" => "all"}
+  #   assert_broadcast "shout", %{"hello" => "all"}
+  # end
 
-  test "broadcasts are pushed to the client", %{socket: socket} do
-    broadcast_from! socket, "broadcast", %{"some" => "data"}
-    assert_push "broadcast", %{"some" => "data"}
-  end
+  # test "broadcasts are pushed to the client", %{socket: socket} do
+  #   broadcast_from! socket, "broadcast", %{"some" => "data"}
+  #   assert_push "broadcast", %{"some" => "data"}
+  # end
 end
