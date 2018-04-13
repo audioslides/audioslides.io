0dd88155d17d6266c71d3e550f49e20a7af41a17
Robin Böhm <robin.boehm@workshops.de>
2018-04-13 14:55:37 +0200

    fix(video/processing): check for audio/video update before video generation

--- lib/platform/video/video_processing.ex
+++ lib/platform/video/video_processing.ex
@@ -48,12 +48,13 @@ defmodule Platform.VideoProcessing do
   def parse_duration(_), do: 0
 
   def generate_video_for_slide(%Lesson{} = lesson, %Slide{} = slide) do
+    image_filename = create_or_update_image_for_slide(lesson, slide)
+    audio_filename = create_or_update_audio_for_slide(lesson, slide)
+
     # Only generate video of audio or video changed
-    #if VideoHelper.generate_video_hash(slide) != slide.video_hash do
+    if VideoHelper.generate_video_hash(slide) != slide.video_hash do
       Logger.info("Slide #{slide.id} Video: need update")
 
-      image_filename = create_or_update_image_for_slide(lesson, slide)
-      audio_filename = create_or_update_audio_for_slide(lesson, slide)
       video_filename = Filename.get_filename_for_slide_video(lesson, slide)
 
       Core.update_slide(slide, %{video_sync_pid: self()})
@@ -68,9 +69,9 @@ defmodule Platform.VideoProcessing do
       Core.update_slide(slide, %{video_sync_pid: nil})
 
       Logger.info("Slide #{slide.id} Video: generated")
-    #else
-    #  Logger.info("Slide #{slide.id} Video: skipped")
-    #end
+    else
+     # Logger.info("Slide #{slide.id} Video: skipped")
+    end
   end
 
   def create_or_update_image_for_slide(lesson, slide) do
