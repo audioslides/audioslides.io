9db47bcb8cba329fe9d996dec31fbbc69a252c59
Robin Böhm <robin.boehm@workshops.de>
2018-04-14 22:14:18 +0200

    feat(video_processing): use Logger.info instead of IO.inspect

--- lib/platform/processing_queue/heavy_task.ex
+++ lib/platform/processing_queue/heavy_task.ex
@@ -1,13 +1,16 @@
 defmodule Platform.ProcessingQueue.HeavyTask do
+  require Logger
+
   use Honeydew.Progress
 
   alias Platform.VideoProcessing
 
   @behaviour Honeydew.Worker
 
-  def work_really_hard(lesson) do
-    IO.puts "I started really hard work!"
+  def generate_video(lesson) do
+    Logger.info "I started to generate a video for lesson #{lesson.id}!"
 
+    # Enum.with_index(lesson.slides), fn {slide, index}
     Enum.each lesson.slides, fn slide ->
       VideoProcessing.generate_video_for_slide(lesson, slide)
       progress("Video #{slide.id} done!")
@@ -15,6 +18,6 @@ defmodule Platform.ProcessingQueue.HeavyTask do
 
     VideoProcessing.merge_videos(lesson)
 
-    IO.puts "My Work is done"
+    Logger.info "I finished the video for lesson #{lesson.id}!"
   end
 end
