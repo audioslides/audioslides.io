4dbfd615f742bca39f1fa28d4cffca6a0a352b50
Robin Böhm <robin.boehm@workshops.de>
2018-04-14 22:52:11 +0200

    test(text_to_speech): mock google token

--- test/platform/speech/google/text_to_speech_test.exs
+++ test/platform/speech/google/text_to_speech_test.exs
@@ -1,24 +1,33 @@
 defmodule Platform.Speech.Google.TextToSpeechTest do
   use ExUnit.Case
-  #use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
+  use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
 
   import Platform.Speech.Google.TextToSpeech
+  import Mock
+
+  alias Goth.Token
 
   doctest Platform.Speech.Google.TextToSpeech
 
-  # describe "get_speech" do
-  #   test "should convert text to speech" do
-  #     use_cassette "google_tts#valid" do
-  #       params = %{
-  #         "language_key" => "en-US",
-  #         "voice_gender" => "male",
-  #         "text" => "TEST"
-  #       }
-  #       response = get_speech(params)
+  setup_with_mocks [
+    {Token, [], [for_scope: fn _ -> {:ok, %{token: "SUPER_TOKEN"}} end]}
+  ] do
+    {:ok, nothing: "yes"}
+  end
 
-  #       # MP3 Header
-  #       assert response =~ <<255, 243, 68, 196, 0, 0, 0, 3, 72>>
-  #     end
-  #   end
-  # end
+  describe "get_speech" do
+    test "should convert text to speech" do
+      use_cassette "google_tts#valid" do
+        params = %{
+          "language_key" => "en-US",
+          "voice_gender" => "male",
+          "text" => "TEST"
+        }
+        response = get_speech(params)
+
+        # MP3 Header
+        assert response =~ <<255, 243, 68, 196, 0, 0, 0, 3, 72>>
+      end
+    end
+  end
 end
